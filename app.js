c152071c1abba4611d2d4f9900649e9d650b0b91
gurelbs <guri240@gmail.com>
2021-03-04 16:14:27 +0200

    domain

--- app.js
+++ app.js
@@ -1,9 +1,63 @@
-const countriesApi = `https://cors-proxy.htmldriven.com/?url=https://restcountries.herokuapp.com/api/v1`
-
+// create & get Element
+const $ = x => document.querySelector(x)
+const $1 = x => document.createElement(x)
+    // api
+const countriesApi = `https://api.codetabs.com/v1/proxy?quest=https://restcountries.herokuapp.com/api/v1`
+    // global vars
+const listOfCountriesByRegion = {}
+const btnWrap = $('.btns')
+const countriesWrap = $('.countries')
+    // fetch
 const fetchAllcountries = () => {
-    fetch(countriesApi)
-        .then(res => res.json())
-        .then(data => console.log(data))
+        fetch(countriesApi)
+            .then(res => res.json())
+            .then(data => {
+                data.forEach(countries => {
+                    !listOfCountriesByRegion[countries.region] ?
+                        listOfCountriesByRegion[countries.region] = [countries.region] :
+                        listOfCountriesByRegion[countries.region].push({
+                            name: countries.name.common,
+                            code: countries.cca2
+                        })
+                });
+                listOfCountriesByRegion['world'] = listOfCountriesByRegion['']
+                delete listOfCountriesByRegion['']
+                logCountriesByRegion()
+                createRegionBtns()
+            })
+    }
+    // logCountries
+const logCountriesByRegion = () => console.log(listOfCountriesByRegion)
+fetchAllcountries()
+
+// create button
+const createRegionBtns = () => {
+    Object.entries(listOfCountriesByRegion).forEach(e => {
+        let btn = $1('button')
+        let region = e[0].toLowerCase()
+        btn.classList.add('btn', region)
+        btn.innerHTML = e[0]
+        btnWrap.appendChild(btn)
+    })
 }
 
-fetchAllcountries()
+const createCountriesNames = e => {
+    listOfCountriesByRegion[e.target.innerHTML].forEach(e => {
+        if (e.name !== undefined) {
+            let countriesLink = $1('a')
+            countriesLink.href = '#'
+            countriesLink.innerHTML = ` ${e.name} `
+            countriesWrap.appendChild(countriesLink)
+        }
+    })
+}
+const handleClickByRegion = e => {
+    if (countriesWrap.textContent === '') {
+        createCountriesNames(e)
+    } else {
+        countriesWrap.innerHTML = ''
+        createCountriesNames(e)
+    }
+}
+
+btnWrap.addEventListener('click', handleClickByRegion)
